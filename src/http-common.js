13c0156803b90e576aebd99518459d7463b87b96
Abhishek-675 <94445399+Abhishek-675@users.noreply.github.com>
2023-01-25 14:28:49 +0530

    gvygt

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL:'https://www.alegralabs.com:5008',
-    baseURL:'https://localhost:5002/',
+    baseURL:'http://localhost:5002/',
     // baseURL:'https://fe7e-49-37-96-187.in.ngrok.io/',
     headers:{
         'Content-type': 'application/json'
