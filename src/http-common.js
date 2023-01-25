d9b8f8460a515f0add3e94f09696eadb6d626ffa
Abhishek-675 <94445399+Abhishek-675@users.noreply.github.com>
2023-01-25 14:26:47 +0530

    to loalhost

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL:'https://www.alegralabs.com:5008',
-    baseURL:'https://fe7e-49-37-96-187.in.ngrok.io/',
+    baseURL:'https://localhost:5002/',
+    // baseURL:'https://fe7e-49-37-96-187.in.ngrok.io/',
     headers:{
         'Content-type': 'application/json'
     }
