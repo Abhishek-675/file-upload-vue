913d6b71f5d006a5b8c6028fc9e5689acf874d22
Abhishek-675 <94445399+Abhishek-675@users.noreply.github.com>
2023-01-25 14:51:32 +0530

    mnksabhu

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -65,7 +65,7 @@
       <div v-for="image in imageInfos" :key="image" class="img-div">
         <img
           class="img"
-          :src="`https://https://0102-49-37-96-187.in.ngrok.io/files/${image}`"
+          :src="`https://www.alegralabs.com:5008/files/${image}`"
           :alt="image"
         />
       </div>
