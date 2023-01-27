0b9ba55ef33e4e304a7dd03e627eb6ae2c3b2be3
Abhishek-675 <94445399+Abhishek-675@users.noreply.github.com>
2023-01-27 13:36:56 +0530

    5008 to 5007

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -65,7 +65,7 @@
       <div v-for="image in imageInfos" :key="image" class="img-div">
         <img
           class="img"
-          :src="`https://www.alegralabs.com:5008/files/${image}`"
+          :src="`https://www.alegralabs.com:5007/files/${image}`"
           :alt="image"
         />
       </div>
